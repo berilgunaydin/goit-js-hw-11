3ea5d15e7e17168b10e86cf6a7b45272988389ed
Beril Günaydın <berilgnydn2005@gmail.com>
2025-07-13 22:04:53 +0300

    hatadüzeltildi

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,12 +4,13 @@ const publicKey = '50406529-48aaa3d05a76e0acf14414e94';
 const url = 'https://pixabay.com/api/';
 
 export function fetchImages(query) {
-    const currentURL = `${url}?key=${publicKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=20`
+    const currentURL = `${url}?key=${publicKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=20`;
+    
     return axios.get(currentURL)
         .then((response) => {
-            return response;
+            return response.data; // ✅ Sadece veriyi döndürüyoruz
         })
         .catch((error) => {
-            throw new Error(error)
-        })
+            throw new Error(error.message); // ✅ Hatanın mesajını koruyoruz
+        });
 }
